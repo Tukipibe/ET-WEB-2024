5e35c1b45d7d6a07ee95401b3bfd337220ec507e
Tukipibe <peraltajavier143@gmail.com>
2024-10-11 18:00:09 -0300

    hola mundo
    vistas con path

--- core/views.py
+++ core/views.py
@@ -4,40 +4,26 @@ from .models import *
 from django.contrib.auth.views import logout_then_login
 from .forms import *
 
-def comprar(request):
-    if not request.user.is_authenticated:
-        return redirect(to="login")
-    carro = request.session.get("carro",[])
-    total = 0
-    for item in carro:
-        total += item[4]
-    venta = Venta()
-    venta.cliente = request.user
-    venta.total = total
-    venta.save()
-    for item in carro:
-        detalle = DetalleVenta()
-        detalle.producto = Producto.objects.get(codigo = item[0])
-        detalle.precio = item[2]
-        detalle.cantidad = item[3]
-        detalle.venta = venta
-        detalle.save()
-        request.session["carro"] = []
-    return redirect(to="carrito")
-
-    
-def carrito(request):
-    return render(request, 'carrito.html', {"carro":request.session.get("carro",[])})
-
 def home(request):
-    libros = Producto.objects.all
-    return render(request, 'index.html', {'libros':libros, "carro":request.session.get("carro",[])})
+    return render(request,'index.html')
+
+def inicio(request):
+    return render(request,'inicio.html')
+
+def nosotros(request):
+    return render(request,'nosotros.html')
+
+def proyectos(request):
+    return render(request,'proyectos.html')
+
+def contactos(request):
+    return render(request,'contactos.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request,'login.html')
 
 def logout(request):
-    return logout_then_login(request,login_url= 'login')
+    return render(request,login_url='login')
 
 def registro(request):
     if request.method == "POST":
@@ -45,46 +31,6 @@ def registro(request):
         if registro.is_valid():
             registro.save()
             return redirect(to="login")
-    else:
-        registro = Registro()
-    return render(request, 'registro.html', {'form':registro})
-
-def venta(request):
-    return render(request, 'top_ventas.html')
-
-def formu(request):
-    return render(request, 'formulario.html')
-
-def recom(request):
-    return render(request, 'recomendaciones.html')
-
-
-def addtocar(request,codigo):
-    producto = Producto.objects.get(codigo=codigo)
-    carro = request.session.get("carro",[])
-    for item in carro:
-        if item[0] == codigo:
-            item[3] +=1
-            item[4] = item[2] * item[3]
-            break
-    else:
-        carro.append([codigo, producto.nombre, producto.precio, 1,producto.precio, producto.imagen])
-    request.session["carro"] = carro
-    return redirect(to="home")
-
-def dropitem(request,codigo):
-    carro = request.session.get("carro",[])
-    for item in carro:
-        if item[0] == codigo:
-            if item[3]>1:
-                item[3] -=1
-                item[4] = item[2] * item[3]
-                break
-            else:
-                carro.remove(item)
-    request.session["carro"] = carro
-    return redirect(to="carrito")
-
-def limpiar(request):
-    request.session.flush()
-    return redirect(to="home")
+        else:
+            registro = Registro()
+        return render(request, 'registro.html',{'form':registro})
